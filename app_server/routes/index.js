6b9b4716e6b496cc845f88b96b7722d9719d9e8b
stoneniqiu <stonzrj@163.com>
2016-05-31 23:49:58 +0800

    初始化
    全静态网站

---
+++ app_server/routes/index.js
@@ -0,0 +1,12 @@
+﻿var express = require('express');
+var router = express.Router();
+var homeController = require('../controllers/home');
+
+router.get('/', homeController.index);
+router.get('/about', homeController.about);
+router.get('/books', homeController.books);
+router.get('/detail/:id', homeController.detail);
+
+module.exports = router;
+
+
